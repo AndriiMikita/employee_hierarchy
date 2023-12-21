8193d3bbf5abf2f1ab37475aad336d1718f4c05e
AndriiMikita <103601417+AndriiMikita@users.noreply.github.com>
2023-12-21 13:30:06 +0200

    Added ordering when filtering

--- employees/filters.py
+++ employees/filters.py
@@ -1,4 +1,4 @@
-from django_filters import FilterSet, CharFilter, DateFilter
+from django_filters import FilterSet, CharFilter, DateFilter, OrderingFilter
 from .models import Employee
 
 class EmployeeFilter(FilterSet):
@@ -6,6 +6,13 @@ class EmployeeFilter(FilterSet):
     position = CharFilter(lookup_expr='icontains')
     email = CharFilter(lookup_expr='icontains')
     hire_date = DateFilter()
+    order_by = OrderingFilter(
+        fields=('full_name', 'position', 'email', 'hire_date',),
+        field_labels={'hire_date': 'Experience',
+                      'full_name': 'Full Name',
+                      'position': 'Position',
+                      'email': 'Email',},
+    )
 
     class Meta:
         model = Employee
